8bb337d73db54b6331273ee3a5cffc5ad131e3e2
Emanuele <emanuele_valentini@hotmail.it>
2023-03-02 13:27:09 +0100

    TODO
    added TODO

--- src/main/java/it/corso/mygym/repositories/UserRepository.java
+++ src/main/java/it/corso/mygym/repositories/UserRepository.java
@@ -2,8 +2,16 @@ package it.corso.mygym.repositories;
 
 import it.corso.mygym.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository <User,Long> {
+    List<User> findByActiveFlagTrue();
+
+    // TODO: find by activeFlag=true AND have an active subscription --> @query
+    @Query("SELECT u FROM User u where activeFlag = true && u.subscription.endDate < currentDate")
+    List<User> findByActiveFlagTrueAndSubscriptionActive();
 }
