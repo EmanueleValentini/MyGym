30a835baea0af1d123969d170fdc6ee9ac01b39c
Emanuele <emanuele_valentini@hotmail.it>
2023-03-03 17:08:04 +0100

    Update
    POM & Configs

--- src/main/java/it/corso/mygym/controller/UserController.java
+++ src/main/java/it/corso/mygym/controller/UserController.java
@@ -1,5 +1,10 @@
 package it.corso.mygym.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.corso.mygym.model.User;
 import it.corso.mygym.model.dto.UserDto;
 import it.corso.mygym.model.exceptions.UserNotFoundException;
@@ -11,6 +16,18 @@ import java.util.Optional;
 
 @RequestMapping("/users")
 public interface UserController {
+    @Operation(
+            summary = "Create a new user record",
+            description = "Use this resource to add a new user record.",
+            tags = {"user-service"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User record created.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad or malformed request"),
+            @ApiResponse(responseCode = "403", description = "User is not entitled to create this User record."),
+            @ApiResponse(responseCode = "412", description = "User record creation failed, due to validations."),
+            @ApiResponse(responseCode = "500", description = "An unexpected error has occurred. User record was not created.")})
     @PostMapping()
     ResponseEntity<User> save(@RequestBody UserDto userDto);
 
